9f212193278dd55d5e5f4b1d33b8f4ecec614cdb
Julian Wachholz <julian@wachholz.ch>
2018-08-17 16:03:08 +0200

    New simple design

--- html/script.js
+++ html/script.js
@@ -1,5 +1,6 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-50657908-1');
-
+if (navigator.doNotTrack !== "1") {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-50657908-1');
+}
