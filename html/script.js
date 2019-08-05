0139b6306ab55be2ac0ab18f56a28f479c2448b2
Julian Wachholz <julian.wachholz@styx7.com>
2019-08-05 15:03:43 +0200

    New content

--- html/script.js
+++
@@ -1,6 +0,0 @@
-if (navigator.doNotTrack !== "1") {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-50657908-1');
-}
